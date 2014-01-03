89dda9c810706b21803af794b28167d210431861
Marlin Cremers <m.cremers@mms-projects.net>
2014-01-04 00:08:48 +0100

    Added some more log types to the Wood collection

--- src/main/java/net/mms_projects/minecraft_info/items/fake/Wood.java
+++ src/main/java/net/mms_projects/minecraft_info/items/fake/Wood.java
@@ -1,12 +1,16 @@
 package net.mms_projects.minecraft_info.items.fake;
 
 import net.mms_projects.minecraft_info.items.MultiItem;
-import net.mms_projects.minecraft_info.items.blocks.wood.logs.OakWood;
+import net.mms_projects.minecraft_info.items.blocks.wood.logs.*;
 
 public class Wood extends MultiItem {
 
     public Wood() {
-        this.setChoppable(true);
         this.addActualItem(OakWood.class);
+        this.addActualItem(SpruceWood.class);
+        this.addActualItem(BirchWood.class);
+        this.addActualItem(JungleWood.class);
+        this.addActualItem(AcaciaWood.class);
+        this.addActualItem(DarkOakWood.class);
     }
 }
